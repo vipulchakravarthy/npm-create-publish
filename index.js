3cd32ac8ae368cba7b79faa29a309b41fe8f0896
vipulchakravarthy <saivipul15@msitprogram.net>
2021-07-08 11:59:30 +0530

    published the sample package

---
+++ index.js
@@ -0,0 +1,14 @@
+function isPalindrome(str) {
+    let start = 0;
+    let end = str.length - 1;
+    while (start < end) {
+        if (str.charAt(start) !== str.charAt(end)) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
+
+module.exports = isPalindrome;
